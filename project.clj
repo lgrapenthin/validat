20167415bf2092fcb53623f9f29b99353065858e
lgrapenthin <grapenthin.leon@gmail.com>
2015-07-05 23:50:06 +0200

    gh url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject validat "0.1.0"
   :description "Simple custom validation checkers."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lgrapenthin/validat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
